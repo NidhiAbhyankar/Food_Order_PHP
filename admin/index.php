e5a69e152caeab35def1fc9a8a5f49387bdeabd5
NidhiAbhyka <nidhiabhyankar@mail.com>
2022-02-06 11:53:19 +0530

    first commit

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+ <html>
+ <head>
+    <title>Food Order Website-Homepage</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="../css/admin.css">
+    <body>
+     <div class="menu">
+      <div class="wrapper">
+       menu
+      </div>
+     </div>
+
+     <div class="main-content">
+      <div class="wrapper">
+       menu
+      </div>
+     </div>
+     <div class="footer">
+      <div class="wrapper">
+       <p>2022 All rights reserved, some Resturant Developed By- Nidhi Abhyankar</p>
+      </div>
+     </div>
+
+    </body>
+  </head>
+  
+ </html>
+ 
+
+
